90d441273817b42f44cf6bd37ae14e70b9081d73
Yasmmin Claudino <ymclaudino@gmail.com>
2024-09-01 12:16:57 -0300

    05: Filter

--- app/src/main/java/com/ymmc/mypersonallibrary/ui/viewmodel/BookListViewModel.kt
+++ app/src/main/java/com/ymmc/mypersonallibrary/ui/viewmodel/BookListViewModel.kt
@@ -1,17 +1,13 @@
 package com.ymmc.mypersonallibrary.ui.viewmodel
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.ymmc.backend.network.data.model.BookData
-import com.ymmc.backend.network.data.model.BookResponse
 import com.ymmc.backend.network.data.repository.BookRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import retrofit2.Call
-import retrofit2.Callback
 
 class BookListViewModel(
     private val repository: BookRepository
--- backend/src/main/java/com/ymmc/backend/network/data/repository/BookRepositoryImpl.kt
+++ backend/src/main/java/com/ymmc/backend/network/data/repository/BookRepositoryImpl.kt
@@ -2,9 +2,6 @@ package com.ymmc.backend.network.data.repository
 
 import com.ymmc.backend.network.data.model.BookResponse
 import com.ymmc.backend.network.data.source.BookRepositoryDataSource
-import retrofit2.Call
-import retrofit2.Response
-
 
 class BookRepositoryImpl(
     private val bookRepositoryDataSource: BookRepositoryDataSource
